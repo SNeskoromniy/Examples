a84c7faedcb2c514c1f02480594f3084e9ec0f71
Sergey Neskoromniy <sneskoromniy@gmail.com>
2023-07-21 00:02:26 +0200

    Задача №8 ДЗ

---
+++ Task_8/Program.cs
@@ -0,0 +1,16 @@
+﻿// ДЗ Задача №8 ввести все четные числа от 1 до N
+Console.Write("Введите число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int num1 = int.Parse(userInput1);
+
+int i = 2; //счетчик 
+
+while (i <= num1)
+{
+    if (i % 2 == 0) //проверка на четность
+    {
+        Console.Write($"{i}, ");
+    }
+   
+    i = i + 2;
+}
