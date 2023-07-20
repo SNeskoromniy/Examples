239eaf00d645990488f7f41dd040d4d8fea13652
Sergey Neskoromniy <sneskoromniy@gmail.com>
2023-07-20 21:49:05 +0200

    В папке Task_2 решение Задачи №2 ДЗ

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// ДЗ Задача 2 найти максимальное число из 2-х введеных
+
+Console.Write("Введите число A: ");
+string userInput = Console.ReadLine() ?? "";
+int num1 = int.Parse(userInput);
+
+Console.Write("Введите число Б: ");
+string userInput1 = Console.ReadLine() ?? "";
+int num2 = int.Parse(userInput1);
+
+if (num1 > num2)
+{
+    Console.Write($" max= {num1}");
+}
+else
+{
+    Console.Write($" max= {num2}");
+}
