abf079844fd894b78a193a6a953cc42ad70b2916
Sergey Neskoromniy <sneskoromniy@gmail.com>
2023-07-20 23:30:57 +0200

    Задача №6

---
+++ Task_6/Program.cs
@@ -0,0 +1,15 @@
+﻿// ДЗ Задача №6 ввести число и проверить на четность
+Console.Write("Введите число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int num1 = int.Parse(userInput1);
+//int result = num1 % 2;
+
+if (num1 % 2 == 0) //проверка деления числа на 2 без остатка
+{
+    Console.Write("Число чётное");
+}
+
+else
+{
+   Console.Write("Число не чётное");
+}
